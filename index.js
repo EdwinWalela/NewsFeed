b5ea9793604033cc65cb2c5cf15c7ea441369df4
edwin walela <edwinwalela@gmail.com>
2018-03-28 20:20:41 +0300

    live news,MTV added

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
-
-
+// Declare global variables
 var https = require('https')
 var express = require('express')
-var app = express();
 var fs = require('fs');
 var quote = require('./quotes');
-var port=Number(process.env.PORT || 3000)
-app.set('view engine', 'ejs')
-app.use('/public',express.static('public'));
 
 // Read API key from text file (Synchronously)
 var apiKey = fs.readFileSync('key.txt','utf8')
+// Set port as  global
+var port=Number(process.env.PORT || 3000)
+
+var app = express();
+// Templating using EJS
+app.set('view engine', 'ejs')
+
+//Loading static files(stylesheet)
+app.use('/public',express.static('public'));
 
 //Routes
 app.get('/',function(req,res){
-	res.render('index',{data:quote.quote})
+	const request = https.get(`https://newsapi.org/v2/top-headlines?sources=next-big-future&apiKey=${apiKey}`, response => {
+		let news = " " ;
+		var dt = []
+		var final = [] ;
+		dt.push(quote.quote)
+		if(response.statusCode === 200){
+				response.on('data',info => {
+					news += info.toString();
+				})
+				response.on('end',() => {
+					news = JSON.parse(news);
+					final.push(news)
+					dt.push(final)
+					res.render('index',{data:dt})
+				})
+		} else {
+			res.render('index',{data:quote.quote})
+		}
+	})
 })
 app.get('/feed',function(req,res){
-	var body = ""
+	let body = ""
 	const request= https.get(`https://newsapi.org/v2/top-headlines?sources=${req.query.source}&apiKey=${apiKey}`,response =>{
 		if(response.statusCode===200){
 			response.on('data',data => {
@@ -34,8 +56,5 @@ app.get('/feed',function(req,res){
 		}
 	})
 })
-
-
-
 console.log('listening to port 3000')
 app.listen(port)
